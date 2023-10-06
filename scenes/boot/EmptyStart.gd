f764f354609ad38651ee281f9c65e5e5d18e4d27
Paul McNamee <paulmc121@gmail.com>
2023-10-06 04:02:25 -0600

    made splash screen wait and then load the main menu

--- scenes/boot/EmptyStart.gd
+++ scenes/boot/EmptyStart.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
 func _ready() -> void:
-	var _tree = get_tree().current_scene
 	await get_tree().create_timer(2.0).timeout
 	await SceneManager.goto_scene(SceneManager.StartingSplashLogo)
-	await get_tree().create_timer(2.0).timeout
-	await SceneManager.goto_scene(SceneManager.MainMenu)
