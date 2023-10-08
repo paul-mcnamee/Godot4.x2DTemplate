432e14572e4f21d7e809075b719edacb91e28cbd
Paul McNamee <paulmc121@gmail.com>
2023-10-08 14:26:17 -0600

    implemented uuid into globals mainly as a demonstration

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,6 +1,11 @@
 extends Node
 
+@onready var uuid: UUID = UUID.new()
+
 # TODO: should include defaults here for a nice color palatte so it is consistent.
 var colors: Dictionary = {
 	dark_green = Color("#3D5220")
 }
+
+func _ready():
+	print ("globals uuid = ", uuid.as_string())
